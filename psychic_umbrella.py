78a4332c4f21b067a2f9e24d2afc4471bf11e270
Robert Henderson <balamkej@gmail.com>
2020-08-30 06:38:11 -0700

    switch to pandas

--- psychic_umbrella.py
+++ psychic_umbrella.py
@@ -1,33 +1,34 @@
 import chess.pgn
+import pandas as pd
 
-pgn_file = "test_pgn/myblitz.pgn"
+pgn_file = 'test_pgn/myblitz.pgn'
 
-player = "bkeej"
+player = 'bkeej'
 
 def win(pgn):
-	result = pgn.headers["Result"]
-	if result == "1-0" and pgn.headers["White"] == player:
-		return True
-	elif result == "0-1" and pgn.headers["Black"] == player:
-		return True
+	result = pgn.headers['Result']
+	if pgn.headers['Result'] == '1/2-1/2':
+		return 'draw'
+	elif result == '1-0' and pgn.headers['White'] == player:
+		return 'win'
+	elif result == '0-1' and pgn.headers['Black'] == player:
+		return 'win'
 	else:
-		return False
+		return 'loss'
 
-def draw(pgn):
-	if pgn.headers["Result"] == "1/2-1/2":
-		return True
-	else:
-		return False
+def elo(pgn):
+	if pgn.headers['White'] == player:
+		return pgn.headers['WhiteElo']
+	elif pgn.headers['Black'] == player:
+		return pgn.headers['BlackElo']
 
+df = pd.DataFrame(columns=['Date','Result','Rating','TimeControl'])
 
 with open(pgn_file) as f:
 	pgn = chess.pgn.read_game(f)
 	while pgn:
-		if draw(pgn) == True:
-			print("draw")
-		elif win(pgn) == True:
-			print("win")
-		else:
-			print("loss")
-			
+		row = {'Date':pgn.headers['Date'], 'Result':win(pgn), 'Rating':elo(pgn), 'TimeControl':pgn.headers['TimeControl']}
+		df = df.append(row, ignore_index=True)
 		pgn = chess.pgn.read_game(f)
+
+print(df)
